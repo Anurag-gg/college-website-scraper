f70dd181797ae88bd87dc1b5d4aaa5e94ebfa412
Anurag-gg <86948851+Anurag-gg@users.noreply.github.com>
2021-08-24 15:45:28 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+import time
+
+
+driver = webdriver.Edge(executable_path= "C:\\Program Files\\msedgedriver.exe" , log_path= 'NUL')
+driver.get("http://exam.msrit.edu/index.php")
+
+cap = input("enter the captcha \n")
+
+with open('usn_is.txt' , 'w') as f:
+    for i in range(1,170):
+        element = driver.find_element_by_id("usn")
+        element.send_keys(f"1MS20IS{i:03}-T")
+        element2 = driver.find_element_by_id("osolCatchaTxt0")
+        element2.send_keys(cap)
+
+
+
+        driver.find_element_by_class_name("buttongo").click()
+
+        
+        try:
+            name = driver.find_element_by_tag_name("h3")
+            cgpa = driver.find_elements_by_tag_name("p")
+
+            
+            data = f'1MS20IS{i:03}-T {float(cgpa[4].text):.2f}   {name.text} \n'
+            f.write(data)
+
+            print(cgpa[4].text)
+            print(name.text ) 
+        except Exception as e:
+            print(e)
+        driver.back()
